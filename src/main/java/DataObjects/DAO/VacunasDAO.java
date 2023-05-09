38506399667de671e554db863b46873a69d7e1c7
TaponTV <tapiavazquez.l1518@gmail.com>
2023-05-08 22:07:50 -0600

    Create VacunasDAO and Vacunas model

---
+++ src/main/java/DataObjects/DAO/VacunasDAO.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DataObjects.DAO;
+
+import DataObjects.interfaces.GenericInterface;
+import java.util.List;
+
+/**
+ *
+ * @author eduar
+ */
+public class VacunasDAO implements GenericInterface{
+
+    @Override
+    public boolean create(Object ob) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List read() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean update(Object ob) {
+       return false;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return false;
+    }
+    
+}
