9e0120fd4989a4ed9779147cb905b3a7d9810889
tapontv <tapiavazquez.l1518@gmail.com>
2023-05-31 22:14:14 -0600

    add method read EspecieDAO

--- src/main/java/DataObjects/DAO/EspecieDAO.java
+++ src/main/java/DataObjects/DAO/EspecieDAO.java
@@ -1,11 +1,24 @@
 package DataObjects.DAO;
 
+import DataObjects.DBConnection.ConnectionDB;
 import java.util.List;
 
 import DataObjects.interfaces.especieInterface;
+import java.sql.*;
+import java.util.ArrayList;
 import models.Especie;
 
 public class EspecieDAO implements especieInterface {
+
+    private Connection connect = null;
+    private ResultSet rs = null;
+    private PreparedStatement ps = null;
+    private List<Especie> list;
+    private Especie obj;
+
+    public EspecieDAO() {
+        connect = ConnectionDB.getConnection();
+    }
 
     @Override
     public boolean create(Especie ob) {
@@ -15,8 +28,24 @@ public class EspecieDAO implements especieInterface {
 
     @Override
     public List<Especie> read() {
-        // TODO Auto-generated method stub
-        return null;
+        try {
+            list = new ArrayList<>();
+            ps = connect.prepareStatement("SELECT * FROM especie");
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                obj = new Especie();
+                obj.setIdEspecie(rs.getInt("idespecie"));
+                obj.setEspecie(rs.getString("especie"));
+                list.add(obj);
+            }
+            return list;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            ConnectionDB.closeDB(rs);
+            ConnectionDB.closeDB(ps);
+        }
     }
 
     @Override
