3efe592bdef48b75eb0eb1d18ece4d98c1a380f8
tapontv <tapiavazquez.l1518@gmail.com>
2023-06-03 11:10:24 -0600

    modify interface abstract Methods

--- src/main/java/DataObjects/interfaces/consultaInterface.java
+++ src/main/java/DataObjects/interfaces/consultaInterface.java
@@ -3,5 +3,5 @@ package DataObjects.interfaces;
 import models.Consulta;
 
 public interface consultaInterface extends GenericInterface<Consulta>{
-
+    public int count(String idvet);
 }
--- src/main/java/DataObjects/interfaces/solicitudInterface.java
+++ src/main/java/DataObjects/interfaces/solicitudInterface.java
@@ -5,4 +5,5 @@ import models.Solicitud;
 public interface solicitudInterface extends GenericInterface<Solicitud>{
 
     public boolean update(int id, int status);
+    public int count(String idvet);
 }
